350d004293faf26dbdad1fa4a05d0c1a9d3e477e
Łukasz Pękala <lukasz-pekala@users.noreply.github.com>
2022-06-13 12:30:36 +0200

    Added firstName and lastName fields

--- src/index.js
+++ src/index.js
@@ -5,13 +5,31 @@ import "./styles.css";
 
 const SignupForm = () => {
   const formik = useFormik({
-    initialValues: { email: "" },
-    onSubmit: values => {
+    initialValues: { firstName: "", lastName: "", email: "" },
+    onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     }
   });
   return (
     <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
+
       <label htmlFor="email">Email Address</label>
       <input
         id="email"
