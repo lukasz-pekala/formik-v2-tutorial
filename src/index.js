5cf25e9ab93908b6dcf9f3ccf020bc1dc02b3c44
Łukasz Pękala <lukasz-pekala@users.noreply.github.com>
2022-06-13 20:11:05 +0200

    Refactored to <Field> and <ErrorMessage>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Formik } from "formik";
+import { Field, ErrorMessage, Formik } from "formik";
 import * as Yup from "yup";
 
 import "./styles.css";
@@ -25,37 +25,16 @@ const SignupForm = () => {
       {(formik) => (
         <form onSubmit={formik.handleSubmit}>
           <label htmlFor="firstName">First Name</label>
-          <input
-            id="firstName"
-            name="firstName"
-            type="text"
-            {...formik.getFieldProps("firstName")}
-          />
-          {formik.touched.firstName && formik.errors.firstName ? (
-            <div>{formik.errors.firstName}</div>
-          ) : null}
+          <Field name="firstName" type="text" />
+          <ErrorMessage name="firstName" />
 
           <label htmlFor="lastName">Last Name</label>
-          <input
-            id="lastName"
-            name="lastName"
-            type="text"
-            {...formik.getFieldProps("lastName")}
-          />
-          {formik.touched.lastName && formik.errors.lastName ? (
-            <div>{formik.errors.lastName}</div>
-          ) : null}
+          <Field name="lastName" type="text" />
+          <ErrorMessage name="lastName" />
 
           <label htmlFor="email">Email Address</label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            {...formik.getFieldProps("email")}
-          />
-          {formik.touched.email && formik.errors.email ? (
-            <div>{formik.errors.email}</div>
-          ) : null}
+          <Field name="email" type="email" />
+          <ErrorMessage name="email" />
 
           <button type="submit">Submit</button>
         </form>
